7e107d136cec28c9747102a1715ad72527e525a5
Logan <128956602+LoganSama@users.noreply.github.com>
2023-04-01 15:38:29 +0530

    Till chapter 155

--- Manga/src/Manga/open.java
+++ Manga/src/Manga/open.java
@@ -30,30 +30,32 @@ public class open{
 	static WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(4));
 	public static String imgXpath ="//div[contains(@class,'simplesocialbuttons')]/..//img[contains(@alt,'1')]";
 	public static String folderPath="D:\\MangaLad\\Manga\\Manga\\Manga Chapter Files\\";
+	public static String homePage = "https://w9.jujmanga.com";
+	public static String newPath = null;
 	public static Scanner sc = new Scanner(System.in);
+	static ArrayList<String> newTab = new ArrayList<String>(driver.getWindowHandles());
 	public static void main(String[] args) throws FileNotFoundException, MalformedURLException {
 		// TODO Auto-generated method stub
 		@SuppressWarnings("unused")
-		String driverPath = "D:\\MangaLad\\Manga\\Manga\\Lib\\chromedriver.exe";
-
-		ArrayList<String> newTab = new ArrayList<String>(driver.getWindowHandles());
-		driver.get("https://w9.jujmanga.com/");
+		String driverPath = "D:\\MangaLad\\Manga\\Manga\\Lib\\chromedriver.exe";	
+		driver.get(homePage);
 		driver.manage().window().maximize();
 
-		w.until(ExpectedConditions.presenceOfElementLocated (By.xpath("//*[text()='Jujutsu Kaisen, Chapter 1']"))).click();
-		driver.switchTo().window(newTab.get(0));
-		folderPath=folderPath + "IterateIMG";
-		createFolder(folderPath);
-		iterateIMGNum();
-		//		saveIMGIntoPdf();
-		//driver.close();
+		//		w.until(ExpectedConditions.presenceOfElementLocated (By.xpath("//*[text()='Jujutsu Kaisen, Chapter 1']"))).click();
+		//		driver.switchTo().window(newTab.get(0));
+		//		folderPath=folderPath + "IterateIMG";
+		//		createFolder(folderPath);
+		//		iterateIMGNum();
+		iterateChapters();
+		driver.quit();
 	}
 
-	public static void createFolder(String path) {
-		File file = new File(path);
+	public static void createFolder(int chpNum) {
+		newPath=folderPath + "Chapter " + chpNum;
+		File file = new File(newPath);
 		boolean bool = file.mkdir();
 		if(bool) {
-			System.out.println("Folder Created under" + path);
+			System.out.println("Folder Created under" + newPath);
 		}
 		else {
 			System.out.println("Folder Not Created");
@@ -82,54 +84,83 @@ public class open{
 	} 
 	 * @throws MalformedURLException **/
 
-	public static void iterateIMGNum() throws FileNotFoundException, MalformedURLException {
-		String imgXpath = "//div[contains(@class,'simplesocialbuttons')]/..//img[contains(@alt,'Jujutsu Kaisen, Chapter 1')]";
-		String imgXpath2 = "//div[contains(@class,'simplesocialbuttons')]/..//img[contains(@alt,'Jujutsu Kaisen, Chapter 1 image 00##NUM##')]";
-		String imgXpath3 = "//div[contains(@class,'simplesocialbuttons')]/..//img[contains(@alt,'Jujutsu Kaisen, Chapter 1 image 0##NUM##')]";
-		String pdfPath = folderPath + "\\Test.pdf";
-		Image img = null;
+	public static void iterateIMGNum(int chpNum) throws FileNotFoundException, MalformedURLException {
+		String imgXpath = "//h2[text()='All Chapters']/..//*[contains(text(),'Chapter ##CHPNUM##')]";
+		String imgXpath1 = "//div[contains(@class,'simplesocialbuttons')]/..//img[contains(@alt,'Jujutsu Kaisen, Chapter')]";
+		String imgXpath2 = "//div[contains(@class,'simplesocialbuttons')]/..//img[contains(@alt,'Jujutsu Kaisen, Chapter ##CHPNUM## image 00##NUM##')]";
+		String imgXpath3 = "//div[contains(@class,'simplesocialbuttons')]/..//img[contains(@alt,'Jujutsu Kaisen, Chapter ##CHPNUM## image 0##NUM##')]";
+		String imgXpath4= "//*[contains(@class,'simplesocialbuttons')]/..//img[@alt='Jujutsu Kaisen, Chapter ##CHPNUM## image ##IMGNUM##']";
+		String pdfPath = newPath + "\\Chapter "+chpNum+".pdf";
 		System.out.println(pdfPath);
+		Image img = null;
+		String c = String.valueOf(chpNum);
+		String chpNav = imgXpath.replace("##CHPNUM##", c);
+		System.out.println(chpNav);
+		w.until(ExpectedConditions.presenceOfElementLocated (By.xpath(chpNav))).click();
+		driver.switchTo().window(newTab.get(0));
 		PdfWriter writer = new PdfWriter(pdfPath);
 		PdfDocument pdfDoc = new PdfDocument(writer);
 		Document doc = new Document(pdfDoc);
-		List<WebElement> allimg = driver.findElements(By.xpath(imgXpath));
+		List<WebElement> allimg = driver.findElements(By.xpath(imgXpath1));
 		for(int i=1; i<=allimg.size();i++){
 			String s = String.valueOf(i);
 			if(i<=9) {
-				String temp = imgXpath2.replace("##NUM##", s);
+				String chpXpath = imgXpath2.replace("##CHPNUM##", c);
+				String temp = chpXpath.replace("##NUM##", s);
 				System.out.println(temp);
-				w.until(ExpectedConditions.presenceOfElementLocated (By.xpath(temp)));
-				WebElement imgalt = driver.findElement(By.xpath(temp));
-				String imgSRC = imgalt.getAttribute("src");
-				URL imageURL;
-				try {
-					imageURL = new URL(imgSRC);
-					BufferedImage saveImage = ImageIO.read(imageURL);
-					ImageIO.write(saveImage, "jpg", new File(folderPath + "\\im"+ i+".jpg"));
-				} catch (Exception e) {
+				Boolean exist = driver.findElements(By.xpath(temp)).size() == 0;
+				System.out.println(exist);
+				if(exist==false) {
+					w.until(ExpectedConditions.presenceOfElementLocated (By.xpath(temp)));
+					WebElement imgalt = driver.findElement(By.xpath(temp));
+					String imgSRC = imgalt.getAttribute("src");
+					URL imageURL;
+					try {
+						imageURL = new URL(imgSRC);
+						BufferedImage saveImage = ImageIO.read(imageURL);
+						ImageIO.write(saveImage, "jpg", new File(newPath + "\\img"+ i+".jpg"));
+					} catch (Exception e) {
+					}
+					ImageData imageData = ImageDataFactory.create(newPath + "\\img"+i+".jpg");
+					img = new Image(imageData);
+					doc.add(img);
 				}
 			}
+
 			else if(i>9) {
-				String temp = imgXpath3.replace("##NUM##", s);
+				String chpXpath = imgXpath3.replace("##CHPNUM##", c);
+				String temp = chpXpath.replace("##NUM##", s);
 				System.out.println(temp);
-				w.until(ExpectedConditions.presenceOfElementLocated (By.xpath(temp)));
-				WebElement imgalt = driver.findElement(By.xpath(temp));
-				String imgSRC = imgalt.getAttribute("src");
-				URL imageURL;
-				try {
-					imageURL = new URL(imgSRC);
-					BufferedImage saveImage = ImageIO.read(imageURL);
-					ImageIO.write(saveImage, "jpg", new File(folderPath + "\\im"+ i+".jpg"));
-				} catch (Exception e) {
+				Boolean exist = driver.findElements(By.xpath(temp)).size() == 0;
+				System.out.println(exist);
+				if(exist==false) {
+					w.until(ExpectedConditions.presenceOfElementLocated (By.xpath(temp)));
+					WebElement imgalt = driver.findElement(By.xpath(temp));
+					String imgSRC = imgalt.getAttribute("src");
+					URL imageURL;
+					try {
+						imageURL = new URL(imgSRC);
+						BufferedImage saveImage = ImageIO.read(imageURL);
+						ImageIO.write(saveImage, "jpg", new File(newPath + "\\img"+ i+".jpg"));
+					} catch (Exception e) {
+					}
+					ImageData imageData = ImageDataFactory.create(newPath + "\\img"+i+".jpg");
+					img = new Image(imageData);
+					doc.add(img);
 				}
 			}
 			System.out.println("Value of i" + i);
+			}
+		doc.close();
 		}
-		for(int i=1;i<=allimg.size();i++) {
-			ImageData imageData = ImageDataFactory.create(folderPath + "\\im"+i+".jpg");
-			img = new Image(imageData);
-			doc.add(img);
-			
-		}doc.close();
+	public static void iterateChapters() throws FileNotFoundException, MalformedURLException {
+		String chpXpath = "//h2[text()='All Chapters']/..//*[contains(text(),'Jujutsu Kaisen, Chapter')]";
+		List<WebElement> allchp = driver.findElements(By.xpath(chpXpath));
+		for(int j=1; j<allchp.size();j++) {
+			createFolder(j);
+			iterateIMGNum(j);
+			driver.navigate().to(homePage);
+			driver.switchTo().window(newTab.get(0));
+		}
 	}
 }
